512cbc2e7ae609859ef4a57af864a74830279cba
黄乘风 <huangchengfeng@huangchengfengdemacbook-pro.local>
2018-06-30 18:13:11 +0800

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by 黄乘风 on 2018/6/30.
+//  Copyright © 2018年 黄乘风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by 黄乘风 on 2018/6/30.
+//  Copyright © 2018年 黄乘风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
